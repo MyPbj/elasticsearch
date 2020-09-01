9ede624955c0ac3171f12fa706910a08c2d1f49a
e <pbj2824489304@163.com>
2020-09-01 15:17:05 +0800

    更改后的

--- src/main/java/com/pbj/pojo/EduExamqu.java
+++ src/main/java/com/pbj/pojo/EduExamqu.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+import java.util.List;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -15,7 +17,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author PBJ
@@ -45,58 +47,71 @@ public class EduExamqu implements Serializable {
      * 学科Id
      */
     @TableField("subId")
+    @Field(type = FieldType.Long)
     private Long subId;
 
     /**
      * 题目类型Id
      */
     @TableField("typeId")
+    @Field(type = FieldType.Long)
     private Long typeId;
 
     /**
      * 材料主干
      */
+    @Field(type = FieldType.Text)
+    //TODO
     private String material;
 
     /**
      * 分数
      */
+    @Field(type = FieldType.Float)
     private Float score;
 
     /**
      * 创建人
      */
     @TableField("createrId")
+    @Field(type = FieldType.Long)
     private Long createrId;
 
     /**
      * 学期
      */
     @TableField("gradeId")
+    @Field(type = FieldType.Long)
     private Long gradeId;
 
     /**
      * 创建时间
      */
     @TableField("createTime")
+    @Field(type = FieldType.Date)
     private Date createTime;
 
     /**
      * 层次
      */
     @TableField("levelId")
+    @Field(type = FieldType.Long)
     private Long levelId;
 
     @TableField("picId")
+    @Field(type = FieldType.Long)
     private Long picId;
 
     @TableField("getLikeNum")
+    @Field(type = FieldType.Integer)
     private Integer getLikeNum;
 
     /**
      * 章节
      */
     @TableField("unitId")
+    @Field(type = FieldType.Text)
+    //TODO
     private String unitId;
 
     @TableField("isNew")
@@ -106,130 +121,160 @@ public class EduExamqu implements Serializable {
      * 学校Id
      */
     @TableField("schoolId")
+    @Field(type = FieldType.Long)
     private Long schoolId;
 
     /**
      * 听力音频地址
      */
     @TableField("listeningURL")
+    @Field(type = FieldType.Text)
     private String listeningURL;
 
     @TableField("actualLevel")
+    @Field(type = FieldType.Long)
     private Long actualLevel;
 
     @TableField("classificationId")
+    @Field(type = FieldType.Text)
     private String classificationId;
 
     /**
      * 难度
      */
     @TableField("flowId")
+    @Field(type = FieldType.Long)
     private Long flowId;
 
     /**
      * 知识点个数
      */
     @TableField("keyPointNum")
+    @Field(type = FieldType.Long)
     private Long keyPointNum;
 
     /**
      * 引用次数
      */
     @TableField("quoteNum")
+    @Field(type = FieldType.Long)
     private Long quoteNum;
 
     /**
      * 下载次数
      */
     @TableField("downloadNum")
+    @Field(type = FieldType.Long)
     private Long downloadNum;
 
     /**
      * 采集zjw题库为1，名师题库为2，采集xkw题库为4，其他题库为0
      */
     @TableField("bankType")
+    @Field(type = FieldType.Integer)
     private Integer bankType;
 
     /**
      * 知识点：2013,201323，
      */
     @TableField("knowledgeCodes")
+    @Field(type = FieldType.Text)
+    //TODO
     private String knowledgeCodes;
 
     @TableField("trueTypeId")
+    @Field(type = FieldType.Long)
     private Long trueTypeId;
 
     /**
      * 收藏次数
      */
     @TableField("collectionNum")
+    @Field(type = FieldType.Long)
     private Long collectionNum;
 
     /**
      * 知识点名称，以逗号隔开
      */
     @TableField("knowledgeNames")
+    @Field(type = FieldType.Text)
     private String knowledgeNames;
 
     /**
      * 题目被使用次数
      */
     @TableField("useNum")
+    @Field(type = FieldType.Long)
     private Long useNum;
 
     /**
      * 题目错误次数
      */
     @TableField("errorNum")
+    @Field(type = FieldType.Long)
     private Long errorNum;
 
     @TableField("trueType")
+    @Field(type = FieldType.Long)
     private Long trueType;
 
     /**
      * 源id
      */
     @TableField("sourceId")
+    @Field(type = FieldType.Integer)
     private Integer sourceId;
 
     /**
      * 源路径
      */
     @TableField("sourceUrl")
+    @Field(type = FieldType.Text)
     private String sourceUrl;
 
     /**
      * 类型名称
      */
     @TableField("sourceTypeName")
+    @Field(type = FieldType.Text)
     private String sourceTypeName;
 
     @TableField("IsSync")
+    @Field(type = FieldType.Boolean)
     private Boolean IsSync;
 
     /**
      * 题目查重Hash值
      */
     @TableField("HashCode")
+    @Field(type = FieldType.Text)
     private String HashCode;
 
     /**
      * 题目类型别名
      */
     @TableField("typeAlise")
+    @Field(type = FieldType.Text)
     private String typeAlise;
 
     /**
      * 临时同步，用完删除
      */
     @TableField("needSync")
+    @Field(type = FieldType.Integer)
     private Integer needSync;
 
     /**
      * 原来题库类型
      */
     @TableField("oldBankType")
+    @Field(type = FieldType.Integer)
     private Integer oldBankType;
+
+    /*小题集合*/
+    @TableField(exist = false)
+    @Field(type = FieldType.Object)
+    private List<EduExamqucont> eduExamqucontList;
 
 
 }
--- src/main/java/com/pbj/service/impl/EduExamquServiceImpl.java
+++ src/main/java/com/pbj/service/impl/EduExamquServiceImpl.java
@@ -4,7 +4,9 @@ package com.pbj.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.pbj.mapper.EduExamquMapper;
+import com.pbj.mapper.EduExamqucontMapper;
 import com.pbj.pojo.EduExamqu;
+import com.pbj.pojo.EduExamqucont;
 import com.pbj.repository.EduExamquRepository;
 import com.pbj.service.EduExamquService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,13 +31,52 @@ public class EduExamquServiceImpl extends ServiceImpl<EduExamquMapper, EduExamqu
     @Autowired
     private EduExamquRepository eduExamquRepository;
 
+    @Autowired
+    private EduExamqucontMapper eduExamqucontMapper;
+
     @Override
     public List<EduExamqu> sync() {
         QueryWrapper<EduExamqu> queryWrapper=new QueryWrapper<>();
         System.out.println(eduExamquMapper.getClass());
-        queryWrapper.eq("id",20488);
+        queryWrapper.eq("gradeId",10);
+        queryWrapper.eq("subId",13);
+//        queryWrapper.eq("id","20488");
         List<EduExamqu> eduExamqus = eduExamquMapper.selectList(queryWrapper);
-        eduExamquRepository.saveAll(eduExamqus);
+        eduExamqus.forEach(item->{
+            QueryWrapper<EduExamqucont> query=new QueryWrapper<EduExamqucont>();
+            query.select("id","cont","prepA","prepB","prepC",
+                    "prepD",
+                    "prepE",
+                    "prepF",
+                    "prepG",
+                    "prepH",
+                    "quId",
+                    "picId",
+                    "isAuto",
+                    "errNum",
+                    "trueFreq",
+                    "useNum",
+                    "errFreq",
+                    "anspicId",
+                    "prepApicId",
+                    "prepBpicId",
+                    "prepCpicId",
+                    "prepDpicId",
+                    "prepEpicId",
+                    "prepFpicId",
+                    "prepGpicId",
+                    "prepHpicId",
+                    "keyPointCode",
+                    "typeId",
+                    "syncType",
+                    "ansImageWidth",
+                    "analysisImageWidth"
+                    );
+            query.eq("quId",item.getId());
+            List<EduExamqucont> eduExamquconts = eduExamqucontMapper.selectList(query);
+            item.setEduExamqucontList(eduExamquconts);
+            EduExamqu save = eduExamquRepository.save(item);
+        });
         return eduExamqus;
     }
 }
